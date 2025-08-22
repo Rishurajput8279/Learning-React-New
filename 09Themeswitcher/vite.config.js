7249ca37b5bde45ff014de8bb2754f2d087e8546
Reshu Jaynt <reshu.22b1531032@abes.ac.in>
2025-08-22 22:58:06 +0530

    change the tailwindconfig.js

--- 09Themeswitcher/vite.config.js
+++ 09Themeswitcher/vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss({
+      config: {
+        darkMode: 'class',  // <-- here, instead of default 'media'
+        theme: {
+          extend: {},
+        },
+        plugins: [],
+      }
+    })
   ],
 })
